8999308843de7d6702ac7943733d1234e32a6670
mengzhiai <458677565@qq.com>
2020-03-15 02:12:17 +0800

    初始化项目

---
+++ src/components/common/Header.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="header">
+  <ul class="flex">
+    <li v-for="(item, i) in navList" :key="i">{{item.name}}</li>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      navList: [{
+        name: '首页'
+      }, {
+        name: '前端'
+      }, {
+        name: '数据库'
+      }, {
+        name: '技术交流'
+      }, {
+        name: '关于我们'
+      }, {
+        name: '留言'
+      }]
+    };
+  }
+};
+</script>
+<style lang="scss" scoped>
+// @import '@/styles/common.scss';
+.header{
+  color: #fff;
+  // background-color: #409EFF;
+  background-color: $theme-color;
+  ul{
+    li{
+      font-size: $fontSize;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header-component></header-component>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderComponent from '@/components/common/Header'
+  export default {
+    components:{
+      HeaderComponent
+    },
+    data(){
+      return{
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+      }
+    }
   }
-}
 </script>
+
+<style lang="scss" scoped>
+
+</style>
